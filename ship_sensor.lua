b8f4fa690db4dbdf002b194278a40ab6eecf212e
Tornc <110984950+Tornc@users.noreply.github.com>
2024-11-21 23:50:51 +0100

    ingame testing pain2

--- ship_sensor.lua
+++ ship_sensor.lua
@@ -1,14 +1,9 @@
 -- Written by Ton, with love. Feel free to modify, consider this under the MIT license.
 
---[[ TESTING ]]
-
-periphemu.create("top", "modem")
-
 --[[ DEPENDENCIES ]]
 package.path = package.path .. ";./modules/?.lua"
 local config = require("config")
 local networking = require("networking")
-local ship = require("fake_ShipAPI")
 local utils = require("utils")
 local pretty = require("cc.pretty")
 
@@ -33,7 +28,7 @@ local SHIPYARD_DIRECTION
 --[[ FUNCTIONS ]]
 
 local function init_settings()
-    local sets = config.load(SETTINGS_FILE_PATH)
+    local sets = config.get_settings(SETTINGS_FILE_PATH)
     if (not sets) or arg[1] == string.lower(ARG_ASK_SETTINGS) then
         -- No need for type conversion; info is all strings anyway.
         config.set_setting(
@@ -52,8 +47,8 @@ local function init_settings()
             "shipyard_direction",
             nil
         )
-        config.save(SETTINGS_FILE_PATH)
-        config.load(SETTINGS_FILE_PATH)
+        config.save_settings(SETTINGS_FILE_PATH)
+        sets = config.get_settings(SETTINGS_FILE_PATH)
     end
     assert(sets, "Settings failed to load somehow.")
     SHIPYARD_DIRECTION = sets["shipyard_direction"]
@@ -61,27 +56,13 @@ local function init_settings()
 end
 
 local function get_orientation()
+    local matrix = ship.getTransformationMatrix()
     local orientation = {
-        ["pitch"] = math.deg(ship.getPitch()),
-        ["yaw"] = math.deg(ship.getYaw()),
-        ["roll"] = math.deg(ship.getRoll()),
+        -- CC:VS's getYaw() implementation does not work at the moment.
+        ["pitch"] = math.deg(math.asin(-matrix[2][3])),
+        ["yaw"] = math.deg(math.atan2(matrix[1][3], matrix[3][3])),
+        ["roll"] = math.deg(math.atan2(matrix[2][1], matrix[2][2])),
     }
-    -- NESW bullshit explanation:
-    -- ShipAPI's orientation will always be based on the orientation a ship is in the shipyard.
-    -- The 'true north' (front) of a ship may not be the same as the front of your build.
-    -- Shift yaw by 90 degrees accordingly. N = +0, E = +90, S = +180, W = +270. - done
-    -- Therefore, if a ship is built facing south, the roll will be inverted. - done
-    -- When it’s east, roll becomes inverted pitch and pitch becomes roll.
-    -- When it's west, roll becomes pitch and pitch becomes inverted roll.
-    orientation["yaw"] = orientation["yaw"] +
-        (90 * (utils.index_of(DIRECTIONS, SHIPYARD_DIRECTION) - 1) + 180) % 360 - 180
-    if SHIPYARD_DIRECTION == "South" then
-        orientation["roll"] = -orientation["roll"]
-    elseif SHIPYARD_DIRECTION == "East" then
-        orientation["roll"], orientation["pitch"] = -orientation["pitch"], orientation["roll"]
-    elseif SHIPYARD_DIRECTION == "West" then
-        orientation["roll"], orientation["pitch"] = orientation["pitch"], -orientation["roll"]
-    end
     return orientation
 end
 
@@ -91,14 +72,6 @@ local function get_omega()
         ["yaw"] = math.deg(ship.getOmega().y),
         ["roll"] = math.deg(ship.getOmega().z),
     }
-    -- Same bullshit here, only that yaw does not need to get shifted.
-    if SHIPYARD_DIRECTION == "South" then
-        omega["roll"] = -omega["roll"]
-    elseif SHIPYARD_DIRECTION == "East" then
-        omega["roll"], omega["pitch"] = -omega["pitch"], omega["roll"]
-    elseif SHIPYARD_DIRECTION == "West" then
-        omega["roll"], omega["pitch"] = omega["pitch"], -omega["roll"]
-    end
     return omega
 end
 
@@ -110,18 +83,20 @@ local function main()
     local display_string = "=][= SHIP SENSOR v" .. VERSION .. " =][="
     print(display_string)
     print(string.rep("-", #display_string))
+    print("Optional program argument:")
+    print("ship_sensor [" .. ARG_ASK_SETTINGS .. "]")
+    print(string.rep("-", #display_string))
     print("ID: " .. MY_ID)
     print("Shipyard direction: " .. SHIPYARD_DIRECTION)
     while true do
         local message = {
             position = ship.getWorldspacePosition(),
-            velocity = ship.getVelocity(),
             orientation = get_orientation(),
-            omega = get_omega()
+            omega = get_omega(),
+            inertia = ship.getMomentOfInertiaTensor()
         }
 
         networking.send_packet(message)
-        ship.run(SLEEP_INTERVAL)
         sleep(SLEEP_INTERVAL)
     end
 end
--- shippeteering.lua
+++ shippeteering.lua
@@ -6,8 +6,8 @@
 
 --[[ TESTING ]]
 
-periphemu.create("top", "modem")
-local function fake_peripheral() end
+-- periphemu.create("top", "modem")
+-- local function fake_peripheral() end
 
 --[[ DEPENDENCIES ]]
 
@@ -24,15 +24,30 @@ local pretty = require("cc.pretty")
 --[[ PERIPHERALS ]]
 
 local MODEM = peripheral.find("modem")
-local REDROUTER_LEOPARD_HULL = { fake_peripheral }
-local REDROUTER_LEOPARD_TURRET = { fake_peripheral }
-local ROT_CONTROLLER_LEOPARD_TURRET = { fake_peripheral }
+local RELAY_TEST_HULL = peripheral.wrap("redstone_relay_3")
+local RELAY_TEST_TURRET = peripheral.wrap("redstone_relay_4")
+local ROT_CONTROLLER_TEST_TURRET = peripheral.wrap("Create_RotationSpeedController_0")
+-- local RELAY_LEOPARD_HULL = { fake_peripheral }
+-- local RELAY_LEOPARD_TURRET = { fake_peripheral }
+-- local ROT_CONTROLLER_LEOPARD_TURRET = { fake_peripheral }
 
 --[[ CONSTANTS / SETTINGS ]]
 
 local INCOMING_CHANNEL, OUTGOING_CHANNEL = 6060, 6060
 local MY_ID = "shippeteer"
 local SLEEP_INTERVAL = 1 / 20
+
+--[[ STATE VARIABLES ]]
+
+local movables = {}
+
+--[[ FUNCTIONS ]]
+
+local function turn_off(relay) for _, side in pairs(rs.getSides()) do relay.setOutput(side, false) end end
+local function timeout(duration) return debug.getinfo(2).currentline, duration end
+local function xz(x, z) return vector.new(x, 0, z) end
+local function xyz(x, y, z) return vector.new(x, y, z) end
+local function add_movable(vehicle) for _, comp in pairs(utils.ensure_is_table(vehicle)) do table.insert(movables, comp) end end
 
 --[[ DEPENDENCIES SETUP ]]
 
@@ -41,77 +56,60 @@ networking.set_modem(MODEM)
 networking.set_channels(INCOMING_CHANNEL, OUTGOING_CHANNEL)
 networking.set_id(MY_ID)
 
-local MOVABLES = {}
-local NAME_LEOPARD_HULL = "leo_hull"
-local NAME_LEOPARD_HULL_DECO = "leo_hull_deco"
-local NAME_LEOPARD_TURRET = "leo_turret"
-local NAME_LEOPARD_TURRET_DECO = "leo_turret_deco"
+--[[ PERIPHERALS SETUP ]]
 
---[[ STATE VARIABLES ]]
-
--- tehe ~ ⭐
-
---[[ FUNCTIONS ]]
-
-local function timeout(duration) return debug.getinfo(2).currentline, duration end
-local function xz(x, z) return vector.new(x, 0, z) end
-local function xyz(x, y, z) return vector.new(x, y, z) end
-local function add_movable(vehicle) for _, comp in pairs(vehicle) do table.insert(MOVABLES, comp) end end
+turn_off(RELAY_TEST_HULL)
+turn_off(RELAY_TEST_TURRET)
+ROT_CONTROLLER_TEST_TURRET.setTargetSpeed(0)
 
 --[[ VEHICLE ASSIGNMENT ]]
 
-local COMPONENT_LEOPARD_HULL_DECO = components.ship().create(NAME_LEOPARD_HULL_DECO, xyz(0, 0, 0))
-local COMPONENT_LEOPARD_HULL = components.hull().create(NAME_LEOPARD_HULL, xyz(1, 1, 1),
-    REDROUTER_LEOPARD_HULL, "freq_front", "freq_left", "freq_right", { "freq_back", "freq_back2" })
-local COMPONENT_LEOPARD_TURRET_DECO = components.ship().create(NAME_LEOPARD_TURRET_DECO, xyz(2, 2, 2))
-local COMPONENT_LEOPARD_TURRET = components.turret().create(NAME_LEOPARD_TURRET, xyz(3, 3, 3),
-    REDROUTER_LEOPARD_TURRET, ROT_CONTROLLER_LEOPARD_TURRET)
+-- local NAME_LEOPARD_HULL = "leo_hull"
+-- local NAME_LEOPARD_HULL_DECO = "leo_hull_deco"
+-- local NAME_LEOPARD_TURRET = "leo_turret"
+-- local NAME_LEOPARD_TURRET_DECO = "leo_turret_deco"
 
-COMPONENT_LEOPARD_HULL.add_child_component(COMPONENT_LEOPARD_HULL_DECO, COMPONENT_LEOPARD_TURRET)
-COMPONENT_LEOPARD_TURRET.add_child_component(COMPONENT_LEOPARD_TURRET_DECO)
-COMPONENT_LEOPARD_TURRET.add_weapon("cannon", "top", false, 5.0)
-COMPONENT_LEOPARD_TURRET.add_weapon("coax_machinegun", { "bottom", "left" }, true, 15)
+-- local COMPONENT_LEOPARD_HULL_DECO = components.ship().create(NAME_LEOPARD_HULL_DECO, xyz(0, 0, 0))
+-- local COMPONENT_LEOPARD_HULL = components.hull().create(NAME_LEOPARD_HULL, xyz(1, 1, 1),
+--     RELAY_LEOPARD_HULL, "freq_front", "freq_left", "freq_right", { "freq_back", "freq_back2" })
+-- local COMPONENT_LEOPARD_TURRET_DECO = components.ship().create(NAME_LEOPARD_TURRET_DECO, xyz(2, 2, 2))
+-- local COMPONENT_LEOPARD_TURRET = components.turret().create(NAME_LEOPARD_TURRET, xyz(3, 3, 3),
+--     RELAY_LEOPARD_TURRET, ROT_CONTROLLER_LEOPARD_TURRET)
 
-local VEHICLE_LEOPARD = { hull = COMPONENT_LEOPARD_HULL, turret = COMPONENT_LEOPARD_TURRET }
-add_movable(VEHICLE_LEOPARD)
+-- COMPONENT_LEOPARD_HULL.add_child_component(COMPONENT_LEOPARD_HULL_DECO, COMPONENT_LEOPARD_TURRET)
+-- COMPONENT_LEOPARD_TURRET.add_child_component(COMPONENT_LEOPARD_TURRET_DECO)
+-- COMPONENT_LEOPARD_TURRET.add_weapon("cannon", "top", false, 5.0)
+-- COMPONENT_LEOPARD_TURRET.add_weapon("coax_machinegun", { "bottom", "left" }, true, 15)
 
--- pretty.pretty_print(COMPONENT_LEOPARD_HULL.get_controls())
+-- local VEHICLE_LEOPARD = { hull = COMPONENT_LEOPARD_HULL, turret = COMPONENT_LEOPARD_TURRET }
+-- add_movable(VEHICLE_LEOPARD)
 
--- local controls = COMPONENT_LEOPARD_HULL.get_controls()
--- for key, links in pairs(controls) do
---     for _, link in pairs(utils.ensure_is_table(links)) do
---         print(key, link)
---     end
--- end
+-- Naming
+local NAME_TEST_HULL = "test_hull"
+local NAME_TEST_TURRET = "test_turret"
 
+-- Component creation
+local COMPONENT_TEST_HULL = components.hull().create(NAME_TEST_HULL, xyz(10, 3, 10),
+    RELAY_TEST_HULL, "front", "left", "right", "back")
+local COMPONENT_TEST_TURRET = components.turret().create(NAME_TEST_TURRET, xyz(10, 4, 10),
+    RELAY_TEST_TURRET, ROT_CONTROLLER_TEST_TURRET)
 
---[[ SCRIPT ]]
+-- Extra assignments
+COMPONENT_TEST_HULL.add_child_component(COMPONENT_TEST_TURRET)
+COMPONENT_TEST_TURRET.add_weapon("autocannon", "front", true, 10)
 
-local function script()
-    puppeteer.test(1, 2, timeout(1))
-    -- puppeteer.path_move_to(test_c2, {
-    --     xz(10, 10),
-    --     { xz(20, 20), true },
-    --     xz(30, 30),
-    -- })
-    -- async.pause_until_terminated(puppeteer.reset(COMPONENT_LEOPARD_HULL))
-    -- async.pause_until_terminated(puppeteer.unfreeze(COMPONENT_LEOPARD_HULL))
-    -- async.pause_until_terminated(puppeteer.freeze(COMPONENT_LEOPARD_HULL))
-    -- pretty.pretty_print(async.get_actions())
-end
+-- Vehicle assignment
+local VEHICLE_TEST = { hull = COMPONENT_TEST_HULL, turret = COMPONENT_TEST_TURRET }
+
+-- Movable registration
+add_movable(VEHICLE_TEST)
 
 --[[ STATE ]]
 
 local function update_information()
-    for _, movable in pairs(MOVABLES) do
+    for _, movable in pairs(movables) do
         movable.update_info(networking.get_message(movable.get_name()))
     end
-    -- term.clear()
-    -- pretty.pretty_print(VEHICLE_LEOPARD.hull.get_info())
-    -- local info = VEHICLE_LEOPARD.turret.get_info()
-    -- if info then
-    --     pretty.pretty_print(info["yaw"])
-    -- end
 end
 
 local function main()
@@ -122,4 +120,31 @@ local function main()
     end
 end
 
+--[[ SCRIPT ]]
+
+local function script()
+    async.pause_until_terminated(puppeteer.reset(VEHICLE_TEST.hull))
+    async.pause_until_terminated(puppeteer.unfreeze(VEHICLE_TEST.hull))
+    sleep(0.5) -- Otherwise stuff will get goofy.
+
+    local path = puppeteer.path_move_to(VEHICLE_TEST.hull,
+        { xz(30, 20), xz(50, 0), xz(30, -25), xz(10, 10) }
+    )
+    local fire_mission = puppeteer.fire_at(VEHICLE_TEST.turret, xz(30, 0),
+        puppeteer.fire, { VEHICLE_TEST.turret, "autocannon", 25 }
+    )
+    async.pause_until_terminated(path, fire_mission)
+    local idle = puppeteer.turret_to_idle(VEHICLE_TEST.turret)
+    local rev = puppeteer.move_to(VEHICLE_TEST.hull, xz(-20, -20), true)
+    async.pause_until_terminated(idle, rev)
+    puppeteer.reset(VEHICLE_TEST.hull)
+    print("Done.")
+end
+
 parallel.waitForAll(main, script, networking.message_handler)
+
+-- commands.exec("vs set-static test_hull false")
+-- commands.exec("setblock ~ ~5 ~ minecraft:stone")
+
+--- @TODO: tune LQR further --> 
+--- @BUG: firing mission + movement at same time with same relay, causes weirdness.
